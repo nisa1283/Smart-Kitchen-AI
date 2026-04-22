03d566801c6be459cad498800432acd7aa903b0a
nisa1283 <nisaunal21@istanbularel.edu.tr>
2026-04-22 23:23:22 +0300

    Gereksiz dosyalar kaldırıldı

--- src/download_dataset.py
+++
@@ -1,8 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="XYLhSqvakO4wq7sgITie")
-project = rf.workspace("fridgeingredients").project("fridge-object")
-version = project.version(3)
-dataset = version.download("yolov8", location="dataset")
-
-print("✅ Dataset indirildi:", dataset.location)
